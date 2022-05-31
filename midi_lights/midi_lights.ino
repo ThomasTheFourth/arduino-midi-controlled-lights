ac7cefb47f69e440701dd5d969cae74262a2214a
Thomas Barnwell <thomas@krakenlabs.io>
2022-05-31 13:19:13 -0400

    Commiting correct code

--- midi_lights/midi_lights.ino
+++ midi_lights/midi_lights.ino
@@ -1,384 +1,148 @@
-#include <FastLED.h>
 #include <MIDIUSB.h>
+#include <FastLED.h>
 
 #define LED_PIN 7
-#define NUM_LEDS 600
+#define NUM_LEDS 360
 #define ARRAY_SIZE 30
-
-// Keys
-// 0-10 - Activate tiers bottom to top at full brightness
-// 12-22 - Activate tiers bottom to top at half 75% brightness
-// 24-32 - Activate tiers bottom to top at 50% brightness
-// 34-36 - Change primary color
-
 CRGB leds[NUM_LEDS];
-bool* keys = new bool[12] {false, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int color = 0;
 
 void setup() {
-  Serial.begin(1200);
-  while (!Serial);
-  //  Serial.print("I am in setup");
+  Serial.begin(115200);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.clear();
+  FastLED.show();
 }
-
-// 34-36 - Change primary color
-int primaryColor() {
-  if (keys[34] == 1) {
-    return HUE_RED;
-  }
-  if (keys[35] == 1) {
-    return HUE_ORANGE;
-  }
-  if (keys[36] == 1) {
-    return HUE_YELLOW;
-  }
-  if (keys[37] == 1) {
-    return HUE_GREEN;
-  }
-  if (keys[38] == 1) {
-    return HUE_AQUA;
-  }
-  if (keys[39] == 1) {
-    return HUE_BLUE;
-  }
-  if (keys[40] == 1) {
-    return HUE_PURPLE;
-  }
-  if (keys[40] == 1) {
-    return HUE_PINK;
-  }
-  if (keys[41] == 1) {
-    return random(0, 255);
-  }
-  return 255;
-}
-
-int lookupTable(String code) {
-
-  if (code == "3c") {
-    Serial.println("returning 0");
-    return 0;
-  }
-  if (code == "01") {
-    return 1;
-  }
-  if (code == "02") {
-    return 2;
-  }
-  if (code == "03") {
-    return 3;
-  }
-  if (code == "04") {
-    return 4;
-  }
-  if (code == "05") {
-    return 5;
-  }
-  if (code == "06") {
-    return 6;
-  }
-  if (code == "07") {
-    return 7;
-  }
-  if (code == "08") {
-    return 8;
-  }
-  if (code == "09") {
-    return 9;
-  }
-  if (code == "0A") {
-    return 10;
-  }
-  if (code == "0B") {
-    return 11;
-  }
-  if (code == "0C") {
-    return 12;
-  }
-  if (code == "0D") {
-    return 13;
-  }
-  if (code == "0E") {
-    return 14;
-  }
-  if (code == "0F") {
-    return 15;
-  }
-  if (code == "10") {
-    return 16;
-  }
-  if (code == "11") {
-    return 17;
-  }
-  if (code == "12") {
-    return 18;
-  }
-  if (code == "13") {
-    return 19;
-  }
-  if (code == "14") {
-    return 20;
-  }
-  if (code == "15") {
-    return 21;
-  }
-  if (code == "16") {
-    return 22;
-  }
-  if (code == "17") {
-    return 23;
-  }
-  if (code == "18") {
-    return 24;
-  }
-  if (code == "19") {
-    return 25;
-  }
-  if (code == "19") {
-    return 25;
-  }
-  if (code == "1A") {
-    return 26;
-  }
-  if (code == "1B") {
-    return 27;
-  }
-  if (code == "1C") {
-    return 28;
-  }
-  if (code == "1D") {
-    return 29;
-  }
-  if (code == "1E") {
-    return 30;
-  }
-  if (code == "1F") {
-    return 31;
-  }
-  //  if (code == "20") {
-  //    return 32;
-  //  }
-  //  if (code == "21") {
-  //    return 33;
-  //  }
-  //  if (code == "22") {
-  //    return 34;
-  //  }
-  //  if (code == "23") {
-  //    return 35;
-  //  }
-  //  if (code == "24") {
-  //    return 36;
-  //  }
-  //  if (code == "25") {
-  //    return 37;
-  //  }
-  //  if (code == "26") {
-  //    return 38;
-  //  } if (code == "27") {
-  //    return 39;
-  //  }
-  //  if (code == "28") {
-  //    return 40;
-  //  }
-  //  if (code == "29") {
-  //    return 41;
-  //  }
-  //  if (code == "2A") {
-  //    return 42;
-  //  }
-  //  if (code == "2B") {
-  //    return 43;
-  //  }
-  //  if (code == "2C") {
-  //    return 44;
-  //  }
-  //  if (code == "2D") {
-  //    return 45;
-  //  } if (code == "2E") {
-  //    return 46;
-  //  }
-  //  if (code == "2F") {
-  //    return 47;
-  //  }
-  //  if (code == "30") {
-  //    return 48;
-  //  }
-  //  if (code == "31") {
-  //    return 49;
-  //  }
-  //  if (code == "32") {
-  //    return 50;
-  //  }
-  //  if (code == "33") {
-  //    return 51;
-  //  }
-  //  if (code == "34") {
-  //    return 52;
-  //  } if (code == "35") {
-  //    return 53;
-  //  }
-  //  if (code == "36") {
-  //    return 54;
-  //  }
-  //  if (code == "37") {
-  //    return 55;
-  //  }
-  //  if (code == "38") {
-  //    return 56;
-  //  }
-  //  if (code == "39") {
-  //    return 57;
-  //  }
-  //  if (code == "3A") {
-  //    return 58;
-  //  }
-  //  if (code == "3B") {
-  //    return 59;
-  //  } if (code == "3C") {
-  //    return 60;
-  //  }
-  //  if (code == "3D") {
-  //    return 61;
-  //  }
-  //  if (code == "3E") {
-  //    return 62;
-  //  }
-  //  if (code == "3F") {
-  //    return 63;
-  //  }
-  //  if (code == "40") {
-  //    return 64;
-  //  }
-  //  if (code == "41") {
-  //    return 65;
-  //  }
-  //  if (code == "42") {
-  //    return 66;
-  //  }
-  //  if (code == "43") {
-  //    return 67;
-  //  }
-  //  if (code == "44") {
-  //    return 68;
-  //  }
-  //  if (code == "45") {
-  //    return 69;
-  //  }
-  //  if (code == "46") {
-  //    return 70;
-  //  }
-  //  if (code == "47") {
-  //    return 71;
-  //  }
-  //  if (code == "48") {
-  //    return 72;
-  //  }
-}
-
 
 void loop() {
-//  randomColors();
+  //listen for new MIDI messages
   midiEventPacket_t rx = MidiUSB.read();
-  switch (String(rx.byte2, HEX)) {
-    case "3c":            //Note On message
-      Serial.println("hit");
-      break;
-    default:
-      break;
-  }
+  processMidi(rx);
+  symbol();
 
-  //  midiEventPacket_t rx;
-  //  do {
-  //    //     randomColors();
-  //    //    Serial.println("loop");
-  //    rx = MidiUSB.read();
-  //    if (rx.header != 0) {
-  //
-  //
-  //      //      Serial.print(rx.byte2, HEX);
-  //      String keyPressedCode = String(rx.byte2, HEX);
-  //      randomColors();
-  //      for (int i = 0; i < 599; i++)
-  //      {
-  //        Serial.println("");
-  //        Serial.print("  |----");
-  //        Serial.print(i);
-  //        Serial.print("-");
-  //        Serial.print(leds[i]);
-  //        Serial.print("----|   ");
-  //      }
-  //     keys[0] = true;
-  //     lookupTable(keyPressedCode);
-  //      Serial.println(String(keys[0]));
-  //      Serial.println(keyPressedCode);
-  //
-  //      Serial.println(StirlookupTable(keyPressedCode));
-  //      if (keys[lookupTable(keyPressedCode)] == 0) {
-  //        //        Serial.println("keyPressed let go");
-  //        keys[lookupTable(keyPressedCode)] = 1;
-  //      } else {
-  //        //        Serial.println("keyPressed pressed");
-  //        keys[lookupTable(keyPressedCode)] = 0;
-  //      }
-  //    }
-  //
-  //  } while (rx.header != 0);
 }
 
-//void everyOtherEven() {
-//  FastLED.clear();
-//  for (int i = 0; i <= NUM_LEDS - 1; i++) {
-//    if ((i / ARRAY_SIZE) % 2 == 0) {
-//      leds[i] = CHSV(HUE_AQUA, 255, 255);
-//    } else {
-//      leds[i] = CHSV(HUE_AQUA, 255, 100);
-//    }
-//    //    Serial.print("\n");
-//  }
-//  FastLED.show();
-//}
+void symbol() {
+  leds[359] = CHSV(HUE_AQUA, 255, 255); leds[358] = CHSV(HUE_AQUA, 255, 255); leds[357] = CHSV(HUE_AQUA, 255, 255); leds[356] = CHSV(HUE_AQUA, 255, 255); leds[355] = CHSV(HUE_AQUA, 255, 255); leds[354] = CHSV(HUE_AQUA, 255, 255); leds[353] = CHSV(HUE_AQUA, 255, 255); leds[352] = CHSV(HUE_AQUA, 255, 255); leds[351] = CHSV(HUE_AQUA, 255, 255); leds[350] = CHSV(HUE_AQUA, 255, 255); leds[349] = CHSV(HUE_AQUA, 255, 255); leds[348] = CHSV(HUE_AQUA, 255, 255); leds[347] = CHSV(HUE_AQUA, 255, 255); leds[346] = CHSV(HUE_AQUA, 255, 255); leds[345] = CHSV(HUE_AQUA, 255, 255); leds[344] = CHSV(HUE_AQUA, 255, 255); leds[343] = CHSV(HUE_AQUA, 255, 255); leds[342] = CHSV(HUE_AQUA, 255, 255); leds[341] = CHSV(HUE_AQUA, 255, 255); leds[340] = CHSV(HUE_AQUA, 255, 255); leds[339] = CHSV(HUE_AQUA, 255, 255); leds[338] = CHSV(HUE_AQUA, 255, 255); leds[337] = CHSV(HUE_AQUA, 255, 255); leds[336] = CHSV(HUE_AQUA, 255, 255); leds[335] = CHSV(HUE_AQUA, 255, 255); leds[334] = CHSV(HUE_AQUA, 255, 255); leds[333] = CHSV(HUE_AQUA, 255, 255); leds[332] = CHSV(HUE_AQUA, 255, 255); leds[331] = CHSV(HUE_AQUA, 255, 255); leds[330] = CHSV(HUE_AQUA, 255, 255); leds[300] = CHSV(HUE_AQUA, 255, 255); leds[303] = CHSV(HUE_BLUE, 255, 255); leds[304] = CHSV(HUE_BLUE, 255, 255); leds[305] = CHSV(HUE_BLUE, 255, 255); leds[306] = CHSV(HUE_BLUE, 255, 255); leds[307] = CHSV(HUE_BLUE, 255, 255); leds[308] = CHSV(HUE_BLUE, 255, 255); leds[309] = CHSV(HUE_BLUE, 255, 255); leds[310] = CHSV(HUE_BLUE, 255, 255); leds[311] = CHSV(HUE_BLUE, 255, 255); leds[312] = CHSV(HUE_BLUE, 255, 255); leds[313] = CHSV(HUE_BLUE, 255, 255); leds[314] = CHSV(HUE_BLUE, 255, 255); leds[315] = CHSV(HUE_BLUE, 255, 255); leds[316] = CHSV(HUE_BLUE, 255, 255); leds[317] = CHSV(HUE_BLUE, 255, 255); leds[318] = CHSV(HUE_BLUE, 255, 255); leds[319] = CHSV(HUE_BLUE, 255, 255); leds[320] = CHSV(HUE_BLUE, 255, 255); leds[321] = CHSV(HUE_BLUE, 255, 255); leds[322] = CHSV(HUE_BLUE, 255, 255); leds[323] = CHSV(HUE_BLUE, 255, 255); leds[324] = CHSV(HUE_BLUE, 255, 255); leds[325] = CHSV(HUE_BLUE, 255, 255); leds[326] = CHSV(HUE_BLUE, 255, 255); leds[329] = CHSV(HUE_AQUA, 255, 255); leds[299] = CHSV(HUE_AQUA, 255, 255); leds[296] = CHSV(HUE_BLUE, 255, 255); leds[295] = CHSV(HUE_BLUE, 255, 255); leds[294] = CHSV(HUE_BLUE, 255, 255); leds[293] = CHSV(HUE_BLUE, 255, 255); leds[292] = CHSV(HUE_BLUE, 255, 255); leds[291] = CHSV(HUE_BLUE, 255, 255); leds[290] = CHSV(HUE_BLUE, 255, 255); leds[289] = CHSV(HUE_BLUE, 255, 255); leds[288] = CHSV(HUE_BLUE, 255, 255); leds[287] = CHSV(HUE_BLUE, 255, 255); leds[286] = CHSV(HUE_BLUE, 255, 255); leds[285] = CHSV(HUE_BLUE, 255, 255); leds[284] = CHSV(HUE_BLUE, 255, 255); leds[283] = CHSV(HUE_BLUE, 255, 255); leds[282] = CHSV(HUE_BLUE, 255, 255); leds[281] = CHSV(HUE_BLUE, 255, 255); leds[280] = CHSV(HUE_BLUE, 255, 255); leds[279] = CHSV(HUE_BLUE, 255, 255); leds[278] = CHSV(HUE_BLUE, 255, 255); leds[277] = CHSV(HUE_BLUE, 255, 255); leds[276] = CHSV(HUE_BLUE, 255, 255); leds[275] = CHSV(HUE_BLUE, 255, 255); leds[274] = CHSV(HUE_BLUE, 255, 255); leds[273] = CHSV(HUE_BLUE, 255, 255); leds[270] = CHSV(HUE_AQUA, 255, 255); leds[240] = CHSV(HUE_AQUA, 255, 255); leds[253] = CHSV(HUE_BLUE, 255, 255); leds[254] = CHSV(HUE_BLUE, 255, 255); leds[255] = CHSV(HUE_BLUE, 255, 255); leds[256] = CHSV(HUE_BLUE, 255, 255); leds[269] = CHSV(HUE_AQUA, 255, 255); leds[239] = CHSV(HUE_AQUA, 255, 255); leds[226] = CHSV(HUE_BLUE, 255, 255); leds[225] = CHSV(HUE_BLUE, 255, 255); leds[224] = CHSV(HUE_BLUE, 255, 255); leds[223] = CHSV(HUE_BLUE, 255, 255); leds[210] = CHSV(HUE_AQUA, 255, 255); leds[180] = CHSV(HUE_AQUA, 255, 255); leds[193] = CHSV(HUE_BLUE, 255, 255); leds[194] = CHSV(HUE_BLUE, 255, 255); leds[195] = CHSV(HUE_BLUE, 255, 255); leds[196] = CHSV(HUE_BLUE, 255, 255); leds[209] = CHSV(HUE_AQUA, 255, 255); leds[179] = CHSV(HUE_AQUA, 255, 255); leds[166] = CHSV(HUE_BLUE, 255, 255); leds[165] = CHSV(HUE_BLUE, 255, 255); leds[164] = CHSV(HUE_BLUE, 255, 255); leds[163] = CHSV(HUE_BLUE, 255, 255); leds[150] = CHSV(HUE_AQUA, 255, 255); leds[120] = CHSV(HUE_AQUA, 255, 255); leds[133] = CHSV(HUE_BLUE, 255, 255); leds[134] = CHSV(HUE_BLUE, 255, 255); leds[135] = CHSV(HUE_BLUE, 255, 255); leds[136] = CHSV(HUE_BLUE, 255, 255); leds[149] = CHSV(HUE_AQUA, 255, 255); leds[119] = CHSV(HUE_AQUA, 255, 255); leds[106] = CHSV(HUE_BLUE, 255, 255); leds[105] = CHSV(HUE_BLUE, 255, 255); leds[104] = CHSV(HUE_BLUE, 255, 255); leds[103] = CHSV(HUE_BLUE, 255, 255); leds[90] = CHSV(HUE_AQUA, 255, 255); leds[60] = CHSV(HUE_AQUA, 255, 255); leds[63] = CHSV(HUE_BLUE, 255, 255); leds[64] = CHSV(HUE_BLUE, 255, 255); leds[65] = CHSV(HUE_BLUE, 255, 255); leds[66] = CHSV(HUE_BLUE, 255, 255); leds[67] = CHSV(HUE_BLUE, 255, 255); leds[68] = CHSV(HUE_BLUE, 255, 255); leds[69] = CHSV(HUE_BLUE, 255, 255); leds[70] = CHSV(HUE_BLUE, 255, 255); leds[71] = CHSV(HUE_BLUE, 255, 255); leds[72] = CHSV(HUE_BLUE, 255, 255); leds[73] = CHSV(HUE_BLUE, 255, 255); leds[74] = CHSV(HUE_BLUE, 255, 255); leds[75] = CHSV(HUE_BLUE, 255, 255); leds[76] = CHSV(HUE_BLUE, 255, 255); leds[77] = CHSV(HUE_BLUE, 255, 255); leds[78] = CHSV(HUE_BLUE, 255, 255); leds[79] = CHSV(HUE_BLUE, 255, 255); leds[80] = CHSV(HUE_BLUE, 255, 255); leds[81] = CHSV(HUE_BLUE, 255, 255); leds[82] = CHSV(HUE_BLUE, 255, 255); leds[83] = CHSV(HUE_BLUE, 255, 255); leds[84] = CHSV(HUE_BLUE, 255, 255); leds[85] = CHSV(HUE_BLUE, 255, 255); leds[86] = CHSV(HUE_BLUE, 255, 255); leds[89] = CHSV(HUE_AQUA, 255, 255); leds[59] = CHSV(HUE_AQUA, 255, 255); leds[56] = CHSV(HUE_BLUE, 255, 255); leds[55] = CHSV(HUE_BLUE, 255, 255); leds[54] = CHSV(HUE_BLUE, 255, 255); leds[53] = CHSV(HUE_BLUE, 255, 255); leds[52] = CHSV(HUE_BLUE, 255, 255); leds[51] = CHSV(HUE_BLUE, 255, 255); leds[50] = CHSV(HUE_BLUE, 255, 255); leds[49] = CHSV(HUE_BLUE, 255, 255); leds[48] = CHSV(HUE_BLUE, 255, 255); leds[47] = CHSV(HUE_BLUE, 255, 255); leds[46] = CHSV(HUE_BLUE, 255, 255); leds[45] = CHSV(HUE_BLUE, 255, 255); leds[44] = CHSV(HUE_BLUE, 255, 255); leds[43] = CHSV(HUE_BLUE, 255, 255); leds[42] = CHSV(HUE_BLUE, 255, 255); leds[41] = CHSV(HUE_BLUE, 255, 255); leds[40] = CHSV(HUE_BLUE, 255, 255); leds[39] = CHSV(HUE_BLUE, 255, 255); leds[38] = CHSV(HUE_BLUE, 255, 255); leds[37] = CHSV(HUE_BLUE, 255, 255); leds[36] = CHSV(HUE_BLUE, 255, 255); leds[35] = CHSV(HUE_BLUE, 255, 255); leds[34] = CHSV(HUE_BLUE, 255, 255); leds[33] = CHSV(HUE_BLUE, 255, 255); leds[30] = CHSV(HUE_AQUA, 255, 255); leds[0] = CHSV(HUE_AQUA, 255, 255); leds[1] = CHSV(HUE_AQUA, 255, 255); leds[2] = CHSV(HUE_AQUA, 255, 255); leds[3] = CHSV(HUE_AQUA, 255, 255); leds[4] = CHSV(HUE_AQUA, 255, 255); leds[5] = CHSV(HUE_AQUA, 255, 255); leds[6] = CHSV(HUE_AQUA, 255, 255); leds[7] = CHSV(HUE_AQUA, 255, 255); leds[8] = CHSV(HUE_AQUA, 255, 255); leds[9] = CHSV(HUE_AQUA, 255, 255); leds[10] = CHSV(HUE_AQUA, 255, 255); leds[11] = CHSV(HUE_AQUA, 255, 255); leds[12] = CHSV(HUE_AQUA, 255, 255); leds[13] = CHSV(HUE_AQUA, 255, 255); leds[14] = CHSV(HUE_AQUA, 255, 255); leds[15] = CHSV(HUE_AQUA, 255, 255); leds[16] = CHSV(HUE_AQUA, 255, 255); leds[17] = CHSV(HUE_AQUA, 255, 255); leds[18] = CHSV(HUE_AQUA, 255, 255); leds[19] = CHSV(HUE_AQUA, 255, 255); leds[20] = CHSV(HUE_AQUA, 255, 255); leds[21] = CHSV(HUE_AQUA, 255, 255); leds[22] = CHSV(HUE_AQUA, 255, 255); leds[23] = CHSV(HUE_AQUA, 255, 255); leds[24] = CHSV(HUE_AQUA, 255, 255); leds[25] = CHSV(HUE_AQUA, 255, 255); leds[26] = CHSV(HUE_AQUA, 255, 255); leds[27] = CHSV(HUE_AQUA, 255, 255); leds[28] = CHSV(HUE_AQUA, 255, 255); leds[29] = CHSV(HUE_AQUA, 255, 255); FastLED.show();
+}
 
-//void burst() {
-//  for (int x = 100; x <= 255; x++) {
-//    for (int i = 0; i <= NUM_LEDS - 1; i++) {
-//      leds[i] = CHSV(HUE_AQUA, 255, x);
-//
-//    }
-//    FastLED.show();
-//
-//  }
-//}
+void activateColor(int key) {
+  if (key ==  48) {
+    color = HUE_RED;
+  }
+  if (key == 50) {
+    color = HUE_ORANGE;
+  }
+  if (key == 52) {
+    color = HUE_YELLOW;
+  }
+  if (key == 53) {
+    color = HUE_GREEN;
+  }
+  if (key == 55) {
+    color = HUE_AQUA;
+  }
+  if (key == 57) {
+    color = HUE_BLUE;
+  }
+  if (key == 59) {
+    color = HUE_PURPLE;
+  }
+  //  if (key) {
+  //    color = HUE_PINK;
+  //  }
+}
+
+void deactivateColor(int key) {
+  //  Serial.println("deactivating color");
+  color = 0;
+}
+
+void clearRow(int tier) {
+  int tierStart = (tier - 1 ) * ARRAY_SIZE;
+  int tierEnd = tierStart + (ARRAY_SIZE - 1);
+  //  Serial.println("----");
+  //  Serial.println(tierStart);
+  //  Serial.println(tierEnd);
+  for (int i = tierStart; i <= tierEnd; i++) {
+    leds[i] = CHSV(0, 0, 0);
+  }
+  //  for (int i = tierStart + (NUM_LEDS / 2); i <= tierEnd - 1 + (NUM_LEDS / 2); i++) {
+  //    leds[i] = CHSV(0, 0, 0);
+  //  }
+  FastLED.show();
+}
+
+void lightRow(int tier) {
+  int tierStart = (tier - 1) * ARRAY_SIZE;
+  int tierEnd = tierStart + (ARRAY_SIZE - 1);
+  //  Serial.println("lighintg tier");
+  for (int i = tierStart; i <= tierEnd; i++) {
+    leds[i] = CHSV(color, 255,  255);
+  }
+  //    FastLED.show();
+  //  for (int i = tierStart + (NUM_LEDS / 2); i <= tierEnd - 1 + (NUM_LEDS / 2); i++) {
+  //    leds[i] = CHSV(color, 255, 255);
+  //  }
+  //  FastLED.clear();
+  FastLED.show();
+}
+
+void processMidi(midiEventPacket_t rx) {
+  if (rx.header == 0x9) {
+    handleNoteOn(rx.byte1 & 0xF, rx.byte2, rx.byte3);
+  }
+  if (rx.header == 0x8) {
+    handleNoteOff(rx.byte1 & 0xF, rx.byte2, rx.byte3);
+  }
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity) {
+  if (pitch >= 60 && pitch < 70) {
+    clearRow((pitch + 1) - 60);
+  }
+  if (pitch >= 40 && pitch < 60) {
+    deactivateColor(pitch);
+  }
 
 
+  if (pitch == 72) {
+    clear();
+  }
+}
 
-//void solidColor() {
-//  FastLED.clear();
-//  for (int i = 0; i <= NUM_LEDS - 1; i++) {
-//    leds[i] = CHSV(HUE_AQUA, 255, 255);
-//    FastLED.show();
-//  }
-//
-//}
-void randomColors() {
-  FastLED.clear();
-  for (int i = 0; i <= NUM_LEDS - 1; i++) {
-    if (random(0, 10) % 2 == 0) {
-      leds[i] = CRGB ( 100, 100, 100);
-    }
+void lightAllRows() {
+  for (int i = 0; i < NUM_LEDS;  i++) {
+    leds[i] = CHSV(color, 255,  255);
   }
   FastLED.show();
 }
-//
-//void everyOtherOdd() {
-//  FastLED.clear();
-//  for (int i = 0; i <= NUM_LEDS - 1; i++) {
-//    if ((i / ARRAY_SIZE) % 2 == 1) {
-//      leds[i] = CHSV(HUE_AQUA, 255, 255);
-//    } else {
-//      leds[i] = CHSV(HUE_AQUA, 255, 100);
-//    }
-//    //    Serial.print("\n");
-//  }
-//  FastLED.show();
-//}
+
+void clear() {
+  for (int i = 0; i < NUM_LEDS;  i++) {
+    leds[i] = CHSV(0, 0, 0);
+  }
+  FastLED.show();
+}
+
+
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
+  // it is possible to use the actual midi velocity here, just be sure to
+  // double to value because midi is 0-127
+  // and then change digitalWrite to analogWrite
+  Serial.println(pitch);
+  if (velocity > 0) {
+    velocity = HIGH;
+  }
+
+  if (pitch >= 60 && pitch < 70) {
+    lightRow((pitch + 1) - 60);
+  }
+  if (pitch >= 40 && pitch < 60) {
+    activateColor(pitch);
+  }
+
+  if (pitch == 72) {
+    lightAllRows();
+  }
+}
